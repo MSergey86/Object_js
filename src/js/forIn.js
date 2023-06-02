c97d383579fbcf3919a903b40a197b3419669058
MSergey86 <prdx.morozov@mail.ru>
2023-06-02 17:53:24 +0300

    Change fotIn

--- src/js/forIn.js
+++ src/js/forIn.js
@@ -1,25 +1,15 @@
 export default function orderByProps(object, array) {
-  const copy = { ...object };
   const funcObject = {};
-  for (let i = 0; i < array.length; i += 1) {
-    for (const property in object) {
-      if (property === array[i]) {
-        funcObject[array[i]] = object[property];
-        delete copy[property];
-      }
-    }
-  }
-  const arr = Object.entries(copy);
 
-  arr.sort((prev, next) => {
-    if (prev[0] < next[0]) return -1;
-    if (prev[0] > next[0]) return 1;
+  const allKeysNotSort = Object.keys(object);
+  const keysNotSort = allKeysNotSort.filter((key) => array.includes(key));
+  const keysSort = allKeysNotSort.filter((key) => !array.includes(key));
+  keysSort.sort();
+  const allKeysSort = [...keysNotSort, ...keysSort];
+
+  allKeysSort.forEach((key) => {
+    funcObject[key] = object[key];
   });
 
-  for (let i = 0; i < arr.length; i += 1) {
-    const key = arr[i][0];
-    const value = arr[i][1];
-    funcObject[key] = value;
-  }
   return funcObject;
 }
